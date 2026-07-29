5e8748184ea1b1065cd3cca5f491f869bbc9da9f
ChenChi Yeh <timothyeh666@gmail.com>
2026-07-29 20:31:30 +0800

    dev: complete first basic testing functions example

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -3,4 +3,9 @@ import source.my_functions as my_functions
 
 
 def test_add():
-    pass 
+    result = my_functions.add(1,4)
+    assert result == 5
+
+def test_divide():
+    result = my_functions.divide(10,2)
+    assert result == 5
