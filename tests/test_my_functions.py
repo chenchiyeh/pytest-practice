8cc9e4f9045f78e7241c15301e7e8d255c834e29
ChenChi Yeh <timothyeh666@gmail.com>
2026-07-29 20:42:23 +0800

    dev: complete function based test tutorial

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -6,6 +6,17 @@ def test_add():
     result = my_functions.add(1,4)
     assert result == 5
 
+def test_add_strings():
+    result = my_functions.add("i like ", "burgers")
+    assert result == "i like burgers"
+
+    
 def test_divide():
     result = my_functions.divide(10,2)
     assert result == 5
+
+def test_divide_zero():
+    #put in the expected error case to pass
+    with pytest.raises(ValueError):
+        result = my_functions.divide(10,0)
+    
